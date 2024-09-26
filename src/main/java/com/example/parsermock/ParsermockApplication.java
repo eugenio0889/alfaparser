c1a741211296ea5a5c02ffa6682fb844ac7aaab6
Evgeniy Strigo <estrigo@alfabank.ru>
2024-09-26 23:02:03 +0300

    init

---
+++ src/main/java/com/example/parsermock/ParsermockApplication.java
@@ -0,0 +1,13 @@
+package com.example.parsermock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsermockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParsermockApplication.class, args);
+    }
+
+}
